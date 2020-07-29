81817a513e16a01f64ba6db12929f7b4aac0ae16
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-07-29 15:03:13 -0400

    missing something

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
 const Ray = function() {
-  
+const newRay = {
+    length : 0,
+
+    push : function(value){
+        this[this.length] = value
+        this.length++
+    },
+
+    pop : function(){
+        const lastIndex = this.length - 1;
+        const toBeRemoved = this[lastIndex];
+        delete this[lastIndex];
+        this.length--;
+
+        return toBeRemoved;
+    },
+
+    includes : function(value){
+        for(let i = 0; i < this.length; i++){
+            if (this[i] === value){
+                return true;
+            }
+        }
+        return false
+    },
+
+    indexOf : function(value){
+        for(let i = 0; i < this.length; i++){
+            if (this[i] === value){
+                return i;
+            }
+        }
+        return  - 1
+    },
+
+    shift : function(value){
+        const shifts = this.length
+        const lastIndex = this.length - 1; 
+        delete this[lastIndex];
+        this[this.length]
+        this.length--
+        return shifts;
+
+    },
+
+    unshift: function(value){
+        this[this.length] = value;
+        this.length ++;
+    },
+}
+return newRay
 }
 
 
